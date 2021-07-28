27d1e954bc523b2deada299f6419f92289da19e9
2Fac3R <genr1818@gmail.com>
2021-07-28 18:31:02 -0500

    num_visits session variable in index method

--- Luuna/catalog/views.py
+++ Luuna/catalog/views.py
@@ -8,12 +8,17 @@ def index(request):
     # Generate number of products
     num_products = Product.objects.all().count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
+    
     # Render the HTML template index.html with the data in the context variable.
     return render(
         request,
         'index.html',
         context = {
             'num_products': num_products,
+            'num_visits': num_visits
         },
     )
 
