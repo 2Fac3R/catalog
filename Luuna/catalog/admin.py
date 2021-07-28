3482bccf46963c8d7c86d32b63704786c3932aa2
2Fac3R <genr1818@gmail.com>
2021-07-28 14:13:42 -0500

    Product Model Created and Registered in admin.py

--- Luuna/catalog/admin.py
+++ Luuna/catalog/admin.py
@@ -1,14 +1,17 @@
+from catalog.models import Product
 from django.contrib import admin
+from .models import Product
 
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
-
 # Unregister the provided model admin
 admin.site.unregister(User)
 
-# Register out own model admin, based on the default UserAdmin
+# Register our own model admin, based on the default UserAdmin
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     pass
+
+# Register model Product
+admin.site.register(Product)
--- Luuna/catalog/models.py
+++ Luuna/catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    """Model representing a Product"""
+    sku = models.CharField('SKU', max_length=12, unique=True, 
+        help_text = "A stock-keeping unit (SKU) is a scannable bar code, most often seen printed on product labels in a retail store.")
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    brand = models.CharField(max_length=100)
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
