b3fc7ffb02e6a9cad9877bbd6f9376b7c1d66495
2Fac3R <genr1818@gmail.com>
2021-08-07 12:40:26 -0500

    Notification Module Installed

--- Luuna/Luuna/urls.py
+++ Luuna/Luuna/urls.py
@@ -5,9 +5,12 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
+import notifications.urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('inbox/notifications/', include(notifications.urls, namespace='notifications')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
